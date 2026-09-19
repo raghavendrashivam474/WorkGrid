e1f6fafc661c8c736125eb2fc19b1baaa89179b5
Raghavendra Singh <raghavendrashivam474@gmail.com>
2026-09-19 07:50:39 +0530

    chore(tests): add test projects and establish dependency direction

---
+++ tests/WorkGrid.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace WorkGrid.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/WorkGrid.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace WorkGrid.Infrastructure.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
